ffc2f80a881438d98799ef8276f6e6d214c205e5
curtain <1158380326@qq.com>
2018-11-28 10:45:51 +0800

    初步实现spring boot actuator & spring boot admin

---
+++ spring-actuator-client/src/main/java/com/curtain/springactuatorclient/SpringActuatorClientApplication.java
@@ -0,0 +1,12 @@
+package com.curtain.springactuatorclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorClientApplication.class, args);
+	}
+}
