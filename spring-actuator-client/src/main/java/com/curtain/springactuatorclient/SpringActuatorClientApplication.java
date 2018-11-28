01d490494d64d1e9dc846bbd78ebe7c6f093ae82
curtain <1158380326@qq.com>
2018-11-28 16:58:51 +0800

    集成eureka

--- spring-actuator-client/src/main/java/com/curtain/springactuatorclient/SpringActuatorClientApplication.java
+++ spring-actuator-client/src/main/java/com/curtain/springactuatorclient/SpringActuatorClientApplication.java
@@ -2,8 +2,10 @@ package com.curtain.springactuatorclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringActuatorClientApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/curtain/springactuatoradmin/SpringActuatorAdminApplication.java
+++ src/main/java/com/curtain/springactuatoradmin/SpringActuatorAdminApplication.java
@@ -5,6 +5,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +17,8 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-//@EnableDiscoveryClient
+@EnableEurekaServer
+@SpringBootApplication
 public class SpringActuatorAdminApplication {
 
 	public static void main(String[] args) {
