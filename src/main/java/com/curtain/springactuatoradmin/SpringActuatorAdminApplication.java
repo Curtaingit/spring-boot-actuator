228911be401b070bfbd8908d754e404fa27efdaf
curtain <1158380326@qq.com>
2018-11-28 17:31:55 +0800

    修改eureka

--- src/main/java/com/curtain/springactuatoradmin/SpringActuatorAdminApplication.java
+++ src/main/java/com/curtain/springactuatoradmin/SpringActuatorAdminApplication.java
@@ -5,7 +5,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringActuatorAdminApplication {
 
